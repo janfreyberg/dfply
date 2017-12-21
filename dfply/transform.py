585d1c8538d76047cba2c2c4ce376264b162fb3a
janfreyberg <jan.freyberg@gmail.com>
2017-12-21 09:58:48 +0000

    Edit docstrings to below 80 characters
    Many of the docstrings in this file were longer than 80 characters,
    producing flake8 errors. These are now edited down / broken across
    lines.

--- dfply/transform.py
+++ dfply/transform.py
@@ -34,16 +34,19 @@ def mutate_if(df, predicate, fun):
     Args:
         df (pandas.DataFrame): data passed in through the pipe.
         predicate: a function applied to columns that returns a boolean value
-        fun: a function that will be applied to columns where predicate returns True
+        fun: function that will be applied to columns if predicate returns True
 
     Example:
-        diamonds >> mutate_if(lambda col: min(col) < 1 and mean(col) < 4, lambda row: 2 * row) >> head(3)
+        (diamonds
+         >> mutate_if(lambda col: min(col) < 1 and mean(col) < 4,
+                      lambda row: 2 * row)
+         >> head(3)
            carat      cut color clarity  depth  table  price     x     y     z
         0   0.46    Ideal     E     SI2   61.5   55.0    326  3.95  3.98  4.86
         1   0.42  Premium     E     SI1   59.8   61.0    326  3.89  3.84  4.62
         2   0.46     Good     E     VS1   56.9   65.0    327  4.05  4.07  4.62
-        (columns 'carat' and 'z', both having a min < 1 and mean < 4, are doubled, while the
-        other rows remain as they were)
+        (columns 'carat' and 'z', both having a min < 1 and mean < 4, are
+        doubled, while the other rows remain as they were)
     """
     cols = list()
     for col in df:
@@ -55,9 +58,6 @@ def mutate_if(df, predicate, fun):
     df[cols] = df[cols].apply(fun)
     return df
 
-    # df2 = df.copy()
-    # df2[cols] = df2[cols].apply(fun)
-    # return df2
 
 
 @dfpipe
@@ -78,7 +78,9 @@ def transmute(df, *keep_columns, **kwargs):
             what the new column values will be.
 
     Example:
-        diamonds >> transmute(x_plus_y=X.x + X.y, y_div_z=(X.y / X.z)) >> head(3)
+        (diamonds
+         >> transmute(x_plus_y=X.x + X.y, y_div_z=(X.y / X.z))
+         >> head(3))
 
             y_div_z  x_plus_y
         0  1.637860      7.93
